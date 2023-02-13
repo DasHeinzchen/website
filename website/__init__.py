6c31335a1f30e295cc93a86d8f298c87231c93f1
DasHeinzchen <marlon.heinz@outlook.de>
2023-02-14 00:44:29 +0100

    basic homepage setup

--- website/__init__.py
+++ website/__init__.py
@@ -3,7 +3,10 @@ from website.config import Config
 
 
 def create_app(config_class=Config):
-    app = Flask
+    app = Flask(__name__)
     app.config.from_object(Config)
+
+    from website.main.routes import main
+    app.register_blueprint(main)
 
     return app
